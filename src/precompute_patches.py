6761120b267c07012983a13153106bd6732a932e
LauraMoraB <lmoraballestar@gmail.com>
2020-06-01 12:36:09 +0200

    save csv with patches

--- src/precompute_patches.py
+++ src/precompute_patches.py
@@ -1,62 +1,77 @@
 import importlib
 import os
 import sys
-
-from src.dataset.batch_sampler import BratsSampler
+import csv
+from src.dataset.augmentations.brats_augmentations import zero_mean_unit_variance_normalization
+from src.dataset import nifi_volume_utils as nifi_utils
 from src.dataset.nifi_volume_utils import save_nifi_volume
-from torch.utils.data import DataLoader
-from torchvision import transforms as T
+
 
 from src.dataset import dataset_utils
 from src.config import BratsConfiguration
-from src.dataset.brats_dataset import BratsDataset
 from tqdm import tqdm
+import numpy as np
+
+def load_volume(volume_path: str, normalize: bool=True) -> np.ndarray:
+    volume = nifi_utils.load_nifi_volume(volume_path)
+    if normalize:
+        volume = zero_mean_unit_variance_normalization(volume)
+    return volume
+
 
 config = BratsConfiguration(sys.argv[1])
 model_config = config.get_model_config()
 dataset_config = config.get_dataset_config()
 basic_config = config.get_basic_config()
 
-
 patch_size = config.patch_size
-modalities_to_use = {BratsDataset.flair_idx: True, BratsDataset.t1_idx: True, BratsDataset.t2_idx: True,
-                     BratsDataset.t1ce_idx: True}
-
-n_modalities = 4
+patch_size_str = "x".join(list(map(str, patch_size)))
 n_patches = dataset_config.getint("n_patches")
-n_patients_per_batch = 1
+
+data = dataset_utils.read_brats(dataset_config.get("train_csv"))[:3]
+
 sampling_method = importlib.import_module(dataset_config.get("sampling_method"))
-
 sampling_name = dataset_config.get("sampling_method").split(".")[-1]
-method_path = f"/Users/lauramora/Documents/MASTER/TFM/Data/2019/MICCAI_BraTS_2019_Data_Training_patches/{sampling_name}"
+method_path = f"{dataset_config.get('root_path')}/train_patches/{sampling_name}"
+if not os.path.exists(method_path):
+    os.makedirs(method_path)
 
+with open(f"{method_path}/brats20_data.csv", "w") as file:
 
-data, labels = dataset_utils.get_dataset(dataset_config.get("path_train"))
+    writer = csv.writer(file)
+    writer.writerow(["ID","Grade","subject_ID", "Center", "Patch", "Size"])
+    idx = 0
+    for patient in tqdm(data, total=len(data), desc="Computing patches"):
+        patient_path = os.path.join(patient.data_path, patient.patch_name)
+        flair = load_volume(os.path.join(patient_path, patient.flair), True)
+        t1 =    load_volume(os.path.join(patient_path, patient.t1), True)
+        t2 =    load_volume(os.path.join(patient_path, patient.t2), True)
+        t1_ce = load_volume(os.path.join(patient_path, patient.t1ce), True)
+        seg = load_volume(os.path.join(patient_path, patient.seg), False)
 
+        modalities = np.asarray(list(filter(lambda x: (x is not None), [flair, t1, t2, t1_ce])))
 
-train_dataset = BratsDataset(data, labels, modalities_to_use, sampling_method, patch_size, T.Compose([T.ToTensor()]))
-train_sampler = BratsSampler(train_dataset, n_patients_per_batch, n_patches)
-train_loader = DataLoader(dataset=train_dataset, batch_sampler=train_sampler, num_workers=4)
+        for patch in range(0, n_patches):
 
+            patient_patch_name = f"{patient.patient}_p{patch}_{patch_size_str}"
+            patch_path = f"{method_path}/{patient_patch_name}"
+            if not os.path.exists(patch_path):
+                os.makedirs(patch_path)
 
-for patient_id, data_batch, labels_batch in tqdm(train_loader, desc="Computing patches"):
-    patient_base_name = train_dataset.get_patient_info(patient_id)["name"]
+            print(f"Path: {patch_path}")
+            patch_modality, patch_segmentation = sampling_method.patching(modalities, seg, patch_size)
 
-    for i, (patch_modalities, patch_seg) in enumerate(zip(data_batch, labels_batch)):
-        patient_name = f"{patient_base_name}_p{i}_{patch_size[0]}x{patch_size[1]}x{patch_size[2]}"
-        patch_path = f"{method_path}/{patient_name}"
+            seg_path = f"{patch_path}/{patient_patch_name}_seg.nii.gz"
+            flair_path = f"{patch_path}/{patient_patch_name}_flair.nii.gz"
+            t1_path = f"{patch_path}/{patient_patch_name}_t1.nii.gz"
+            t2_path = f"{patch_path}/{patient_patch_name}_t2.nii.gz"
+            t1ce_path = f"{patch_path}/{patient_patch_name}_t1ce.nii.gz"
 
-        if not os.path.exists(patch_path):
-            os.makedirs(patch_path)
+            save_nifi_volume(patch_segmentation, seg_path)
+            save_nifi_volume(patch_modality[0], flair_path)
+            save_nifi_volume(patch_modality[1], t1_path)
+            save_nifi_volume(patch_modality[2], t2_path)
+            save_nifi_volume(patch_modality[3], t1ce_path)
 
-        seg_path = f"{patch_path}/{patient_name}_seg.nii.gz"
-        flair_path = f"{patch_path}/{patient_name}_flair.nii.gz"
-        t1_path = f"{patch_path}/{patient_name}_t1.nii.gz"
-        t2_path = f"{patch_path}/{patient_name}_t2.nii.gz"
-        t1ce_path = f"{patch_path}/{patient_name}_t1ce.nii.gz"
-
-        save_nifi_volume(patch_seg.numpy(), seg_path)
-        save_nifi_volume(patch_modalities[0].numpy(), flair_path)
-        save_nifi_volume(patch_modalities[1].numpy(), t1_path)
-        save_nifi_volume(patch_modalities[2].numpy(), t2_path)
-        save_nifi_volume(patch_modalities[3].numpy(), t1ce_path)
+            writer.writerow([idx, patient.grade, patient.patch_name, patient.center, patient_patch_name, patch_size_str])
+            idx += 1
