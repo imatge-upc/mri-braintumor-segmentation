fix normalizing segmentation, default value to False

src/dataset/brats_dataset.py
@@ -7,8 +7,6 @@ from torch.utils.data import Dataset
 from torchvision import transforms
 
 from src.dataset import nifi_volume_utils as nifi_utils
-from src.dataset.augmentations.brats_augmentations import zero_mean_unit_variance_normalization
-
 
 
 class BratsDataset(Dataset):
@@ -32,6 +30,7 @@ class BratsDataset(Dataset):
         self.sampling_method = sampling_method
         self.patch_size = patch_size
         self.transforms = transforms
+        self.normalize = False
 
     def __len__(self):
         return len(self.data)
@@ -55,18 +54,15 @@ class BratsDataset(Dataset):
         return idx, patch_modality, patch_segmentation
 
 
-    def _load_volume_modality(self, modality_path: str, modality: int, normalize: bool=True):
+    def _load_volume_modality(self, modality_path: str, modality: int):
         if modality in self.modalities_to_use.keys() and self.modalities_to_use[modality]:
-
-            volume = nifi_utils.load_nifi_volume(modality_path)
-            if normalize:
-                volume = zero_mean_unit_variance_normalization(volume)
+            volume = nifi_utils.load_nifi_volume(modality_path, self.normalize)
             return volume
         else:
             return None
 
     def _load_volume_gt(self, seg_mask: str) -> np.ndarray:
-        return nifi_utils.load_nifi_volume(seg_mask)
+        return nifi_utils.load_nifi_volume(seg_mask, normalize=False) # segmentation --> always false
 
 
     def get_patient_info(self, idx):

src/dataset/nifi_volume_utils.py
@@ -15,7 +15,7 @@ def save_nifi_volume(volume:np.ndarray, path:str):
     img.to_filename(path)
 
 
-def load_nifi_volume(filepath: str, normalize: bool=True) -> np.ndarray:
+def load_nifi_volume(filepath: str, normalize: bool=False) -> np.ndarray:
     proxy = nib.load(filepath)
     img = proxy.get_fdata()
     proxy.uncache()

src/dataset/visualization_utils.py
@@ -24,12 +24,12 @@ def plot_3_view(modal, volume_type: np.ndarray, s: int=100, save: bool=True):
         plt.show()
 
 
-def plot_axis_overlayed(modalities: list, segmentation_mask: str, subject: int, axis: str = 'x', save: bool=False):
+def plot_axis_overlayed(modalities: dict, segmentation_mask: str, subject: int, axis: str = 'x', save: bool=False):
     """Save or show figure of provided axis"""
     fig, axes = plt.subplots(len(modalities), 1)
 
-    for i, modality in enumerate(modalities):
-        display = plot_anat(modality, draw_cross=False, display_mode=axis, axes=axes[i], figure=fig, title=subject)
+    for i, (modality_name, modality_path) in enumerate(modalities.items()):
+        display = plot_anat(modality_path, draw_cross=False, display_mode=axis, axes=axes[i], figure=fig, title=modality_name)
         display.add_overlay(segmentation_mask)
 
     if save:
@@ -42,9 +42,10 @@ def plot_axis_overlayed(modalities: list, segmentation_mask: str, subject: int, 
 def plot_brain_batch_per_patient(patient_ids, data, save=True):
     for patient in patient_ids:
         patient = data[patient.item()]
-        patient_modalities = list(map(lambda x: os.path.join(patient.data_path, patient.patient, x), [patient.flair, patient.t2, patient.t1, patient.t1ce]))
-        patient_seg = os.path.join(patient.data_path, patient.patient, patient.seg)
-        plot_axis_overlayed(patient_modalities, patient_seg, patient, axis='x', save=save)
+        patient_modalities = list(map(lambda x: os.path.join(patient.data_path, patient.patch_name, x), [patient.flair, patient.t2, patient.t1, patient.t1ce]))
+        patient_modalities = {"flair": patient_modalities[0],"t2": patient_modalities[1],"t1": patient_modalities[2],"t1ce": patient_modalities[3] }
+        patient_seg = os.path.join(patient.data_path, patient.patch_name, patient.seg)
+        plot_axis_overlayed(patient_modalities, patient_seg, patient.patch_name, axis='x', save=save)
 
 
 def plot_batch_slice(images, gt, save=True):